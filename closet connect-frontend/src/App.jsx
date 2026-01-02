b3d16b00f1d26f11b42ef98be4cfe133ba4caf4e
Aadya Jha <aadyajha_me24b09_002@dtu.ac.in>
2026-01-02 17:20:59 +0530

    Add frontend code ( React+ Vite)

--- closet connect-frontend/src/App.jsx
+++ closet connect-frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MyListings from "./pages/MyListings";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/my-listings" element={<MyListings />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
